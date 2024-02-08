e852e0668311d224a2fe85ed8b9692005eac4d5a
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2024-02-08 13:50:07 +0200

    add project structure

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import StatusLabel, { Status } from './components/status-label';
+import { headers } from 'next/headers';
+
+export default function Home() {
+  console.log(headers());
+  return (
+    <main>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+    </main>
+  );
+}
